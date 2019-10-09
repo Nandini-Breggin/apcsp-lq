63e288fee42cdd6fa587a62a9d50270a2b1a0028
Nandini Breggin <nandinibreggin@gmail.com>
2019-10-09 10:22:46 -0600

    Final Changes

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,69 +1,71 @@
 def three_even(list)
-    
-    number = 0
+
     i = 0
-    count = 0
 
     list.size.times do |num|
-        if list[number] % 2 == 0
-            if list[number] != list[i + 2]
-                if list[number] != list[i - 2]
+        # Changes to integer
+        if num.to_i % 2 == 0
+            # Checks each value
+            if list[i + 1].to_i % 2 == 0
+                # Checks the value and makes sure that the value isn't beyond the ones in the list.
+                if list[i + 2].to_i % 2 == 0 && i + 2 < list.size
                     return true 
                 end
             end
-        else
-            count += 1
         end
         i += 1
     end
-
-    if count == list.size
-        return false
-    else
-        return true
-    end
+    return false
 
 end
 
-# puts three_even([1, 1, 4, 6]) # false
-# puts three_even([4, 2, 6, 5]) # true
-# puts three_even([2, 4, 12, 5]) # true
-# puts three_even([2, 2, 4]) # true
-# puts three_even([]) # false
+puts three_even([2, 1, 3, 5]) # false
+puts three_even([2, 4, 12, 5]) # true
+puts three_even([2, 1, 4, 6]) # false
+puts three_even([1, 4, 6, 4]) # true
+puts three_even([]) # false
 
 
-def sum(numbs)
-    max = 0
-    min = 0
-    sum = 0
-    total = 0
-    numbs.each do |numb|
-        sum = sum + numb
-        if max <= numb
-            max = numb
-        end
-        if numb == numbs[0]
-            min = numb
-        end
-        if min >= numb
-            min = numb
-        end
-        total = total + 1
-    end
-    total = sum
-    return total
-end
-
-# puts sum([1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-
+# # I don't need the sum loop... I over complicated it.
 def bigger_list(list1, list2)
-    list1 = [sum(list1)]
-    list2 = [sum(list2)]
-    total = list1 + list2
-    print total
+    # finding the sum of the first list and the sum of the second list
+    sum1 = list1[0].to_i + list1[1].to_i
+    sum2 = list2[0].to_i + list2[1].to_i
+
+    # seeing which list has the greater sum
+    if sum1 > sum2
+        return list1
+    elsif sum2 > sum1
+        return list2 
+        # returning the first list if the values are the same
+    else
+        return list1
+    end
 end
 
-puts bigger_list([1, 2], [3, 4]) # [3, 4]
-puts bigger_list([1, 7], [4, 4]) # [1, 7]
+print bigger_list([1, 2], [3, 4]) # [3, 4]
+print bigger_list([1, 7], [4, 4]) # [1, 7]
 
+
+# I didn't start series_up on quiz day # This is my code for today 
+def series_up(num)
+    seq = 0
+    list = []
+
+    num.times do |pat|
+        t = 0
+        num.times do |num|
+            list[t + seq] = t + 1 
+            t += 1
+        end
+        # makes it so that it adds on to the first one (grows exponentially)
+        seq += pat + 1
+    end
+    return list
+end
+
+print series_up(1) # [1]
+print series_up(2) # [1, 1, 2]
+print series_up(3) # [1, 1, 2, 1, 2, 3]
+print series_up(4) # [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+print series_up(5) # [1, 1, 2, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 4, 5]
